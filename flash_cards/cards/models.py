4abf2844589801c8efa469993108a459434efaeb
clavergne <lavergne.clement@yahoo.fr>
2024-03-05 13:43:50 +0100

    Fix new card revision date default value + update some tests names

--- flash_cards/cards/models.py
+++ flash_cards/cards/models.py
@@ -1,10 +1,9 @@
-from datetime import date
-
 from django.db import models
+from django.utils import timezone
 
 
 class Card(models.Model):
     question = models.CharField(max_length=1000)
     answer = models.CharField(max_length=1000)
-    revision_date = models.DateField(default=date.today())
+    revision_date = models.DateField(default=timezone.now)
     revised = models.BooleanField(default=False)
