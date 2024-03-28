d84130a55219e8dd3ef1835d4345b4e3b607a84c
clavergne <lavergne.clement@yahoo.fr>
2024-03-28 22:41:59 +0100

    Link card model to user model

--- flash_cards/cards/tests/test_models.py
+++ flash_cards/cards/tests/test_models.py
@@ -3,13 +3,16 @@ from datetime import date, timedelta
 from django.test import TestCase
 
 from flash_cards.cards.models import Card
+from flash_cards.users.tests.factories import UserFactory
 
 
 class TestCardModel(TestCase):
     def test_create_a_card(self):
+        user = UserFactory()
         Card.objects.create(
             question="Quelle est la capitale de la France ?",
             answer="Paris",
+            user=user,
         )
 
         self.assertEqual(Card.objects.count(), 1)
@@ -17,3 +20,4 @@ class TestCardModel(TestCase):
         self.assertEqual(Card.objects.first().answer, "Paris")
         self.assertEqual(Card.objects.first().revision_date, date.today())
         self.assertEqual(Card.objects.first().revision_time_delta, timedelta(days=1))
+        self.assertEqual(Card.objects.first().user, user)
--- functional_tests/test_revision_cards.py
+++ functional_tests/test_revision_cards.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 from flash_cards.cards.models import Card
+from flash_cards.users.tests.factories import UserFactory
 from functional_tests.functional_test import FunctionalTest
 
 
@@ -15,25 +16,45 @@ class TestCardsRevision(FunctionalTest):
         tomorrow = date.today() + timedelta(days=1)
         yesterday = date.today() + timedelta(days=-1)
 
+        user1 = UserFactory(
+            email="clement@example.fr",
+            password="plopplop",
+        )
+
+        user2 = UserFactory(
+            email="nico@example.fr",
+            password="plopplop",
+        )
+
+        Card.objects.create(
+            question="Quel est la capital de la Lettonie",
+            answer="Riga",
+            revision_date=yesterday - timedelta(days=1),
+            user=user2,
+        )
         Card.objects.create(
             question="Quelle est la capitale de la France ?",
             answer="Paris",
             revision_date=tomorrow,
+            user=user1,
         )
         Card.objects.create(
             question="Quelle est la capitale de l'Italie ?",
             answer="Rome",
             revision_date=date.today(),
+            user=user1,
         )
         Card.objects.create(
             question="Qui fût le 9ème César ?",
             answer="Vitellius",
             revision_date=yesterday,
+            user=user1,
         )
         Card.objects.create(
             question="Qui fût le premier César ?",
             answer="César",
             revision_date=tomorrow,
+            user=user1,
         )
 
     def get_date_string(self, date: date) -> str:
