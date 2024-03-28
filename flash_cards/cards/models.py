c2b4ab6637d3955e484c630a03b6a70c0332a023
clavergne <lavergne.clement@yahoo.fr>
2024-03-28 22:41:59 +0100

    Link card model to user model

--- flash_cards/cards/models.py
+++ flash_cards/cards/models.py
@@ -1,7 +1,10 @@
 from datetime import timedelta
 
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
+
+User = get_user_model()
 
 
 class Card(models.Model):
@@ -9,3 +12,7 @@ class Card(models.Model):
     answer = models.CharField(max_length=1000)
     revision_date = models.DateField(default=timezone.now)
     revision_time_delta = models.DurationField(default=timedelta(days=1))
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
