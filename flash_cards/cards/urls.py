e86e2d76d9178d1e205a2f33544e88c0e9f50eda
clavergne <lavergne.clement@yahoo.fr>
2024-04-05 10:56:00 +0200

    Base for user card collection view

--- flash_cards/cards/urls.py
+++ flash_cards/cards/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 
-from flash_cards.cards.views import correction_card_view, new_card_view, revision_card_view, revision_view
+from flash_cards.cards.views import (
+    card_collection_view,
+    correction_card_view,
+    new_card_view,
+    revision_card_view,
+    revision_view,
+)
 
 app_name = "cards"
 
 urlpatterns = [
     path("new", new_card_view, name="new"),
+    path("card_collection", card_collection_view, name="card_collection"),
     path("revision", revision_view, name="revision"),
     path("revision/<int:pk>", revision_card_view, name="revision_card"),
     path("revision/<int:pk>/correction", correction_card_view, name="correction_card"),
