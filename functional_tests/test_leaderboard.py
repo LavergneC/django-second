b414a6aeb18d411eec3ea3f84213402f51423671
Lavergne Clement <clementlavergne172@gmail.com>
2024-05-17 20:08:22 +0200

    Leaderboard functional test

--- functional_tests/test_leaderboard.py
+++ functional_tests/test_leaderboard.py
@@ -7,7 +7,7 @@ from flash_cards.users.tests.factories import UserFactory
 from functional_tests.functional_test import FunctionalTest
 
 
-class TestUserLogin(FunctionalTest):
+class TestLeaderBoard(FunctionalTest):
     def setUp(self):
         super().setUp()
         super().login()
@@ -32,13 +32,13 @@ class TestUserLogin(FunctionalTest):
             question="Quelle est la capitale de l'Italie ?",
             answer="Rome",
             revision_time_delta=timedelta(days=100),
-            user=self.user,
+            user=self.best_user,
         )
         Card.objects.create(
             question="Qui fût le 9ème César ?",
             answer="Vitellius",
             revision_time_delta=timedelta(days=64),
-            user=self.user,
+            user=self.best_user,
         )
 
         # Create a last (bad) user with his cards
@@ -47,19 +47,19 @@ class TestUserLogin(FunctionalTest):
             question="Qui fût le premier César ?",
             answer="César",
             revision_time_delta=timedelta(days=1),
-            user=self.user,
+            user=self.worst_user,
         )
         Card.objects.create(
             question="Quelle hauteur fait la tour eiffel ? (en mètre)",
             answer="330",
             revision_time_delta=timedelta(days=1),
-            user=self.user,
+            user=self.worst_user,
         )
         Card.objects.create(
             question="Quelle largeur fait la tour eiffel ? (en mètre)",
             answer="124",
             revision_time_delta=timedelta(days=2),
-            user=self.user,
+            user=self.worst_user,
         )
 
     def test_user_checks_leaderboard(self):
@@ -72,9 +72,23 @@ class TestUserLogin(FunctionalTest):
         self.assertTrue(self.wait_page("LeaderBoard"))
 
         # He can see users scores ranked
-        table = self.browser.find_element(By.ID, "leader-board_table_id")
-        rows = table.find_elements_by_tag_name("tr")
+        table = self.browser.find_element(By.ID, "leaderboard_table_id")
+        self.assertTrue(True)
 
-        expected_rows = ("Username", "Score", "TBD", "TBD")
+        web_rows = table.find_elements(By.TAG_NAME, "tr")
+        rows = []
+        for row in web_rows:
+            web_cells = row.find_elements(By.TAG_NAME, "td")
+            cells = []
+            for cell in web_cells:
+                cells.append(cell.text)
+            rows.append(cells)
+
+        expected_rows = [
+            ["Names", "Scores"],
+            [self.best_user.name, str(self.best_user.leaderboard_score)],
+            [self.user.name, str(self.user.leaderboard_score)],
+            [self.worst_user.name, str(self.worst_user.leaderboard_score)],
+        ]
 
         self.assertEqual(rows, expected_rows)
